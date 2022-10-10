273a672c14d9d914c0bab076f5cdb20c3e8d3aa0
Alexandru <alexandrun@gmail.com>
2022-10-11 00:47:19 +0100

    Revert "Modify topic name to messages2"
    This reverts commit 9691d790d2755a27bb6c2176a2dd07db0d99031b.

--- src/main/java/com/heroku/kafka/demo/DemoResource.java
+++ src/main/java/com/heroku/kafka/demo/DemoResource.java
@@ -27,7 +27,7 @@ public class DemoResource {
   }
 
   @GET
-  @Path("messages2")
+  @Path("messages")
   @Produces(MediaType.APPLICATION_JSON)
   @Timed
   public List<DemoMessage> getMessages() {
@@ -35,7 +35,7 @@ public class DemoResource {
   }
 
   @POST
-  @Path("messages2")
+  @Path("messages")
   @Consumes(MediaType.APPLICATION_JSON)
   @Produces(MediaType.APPLICATION_JSON)
   @Timed
