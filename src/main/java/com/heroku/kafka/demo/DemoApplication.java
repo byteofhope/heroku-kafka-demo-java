6c6f301475d769aa43a20c3fe006734ab31ee013
Alexandru <alexandrun@gmail.com>
2023-01-24 15:47:37 +0000

    M2A

--- src/main/java/com/heroku/kafka/demo/DemoApplication.java
+++ src/main/java/com/heroku/kafka/demo/DemoApplication.java
@@ -13,6 +13,7 @@ import org.slf4j.LoggerFactory;
 public class DemoApplication extends Application<DemoConfiguration>  {
   private static final Logger LOG = LoggerFactory.getLogger(DemoApplication.class);
 
+  //M2A
   public static void main(String[] args) throws Exception {
     new DemoApplication().run(args);
   }
